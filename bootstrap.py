891fddfc38859e6e7924493304621cda6e57a31b
yveega <yveega@gmail.com>
2025-03-12 11:33:53 +0300

    Add bootstrap function

---
+++ bootstrap.py
@@ -0,0 +1,51 @@
+import numpy as np
+import sys
+import os
+
+if getattr(sys, 'frozen', False):
+    ci_low_before5d = np.loadtxt(os.path.join(sys._MEIPASS, "data\\ci_low_before_5d.txt"))
+    ci_high_before5d = np.loadtxt(os.path.join(sys._MEIPASS, "data\\ci_high_before_5d.txt"))
+else:
+    ci_low_before5d = np.loadtxt("data\\ci_low_before_5d.txt")
+    ci_high_before5d = np.loadtxt("data\\ci_high_before_5d.txt")
+
+P_low_before5d = np.eye(5, dtype=float)
+P_high_before5d = np.eye(5, dtype=float)
+
+
+def generate_trans_probs(comorbidity):
+    if comorbidity <= 0:
+        P_low_before5d[:3] = ci_low_before5d[:3]
+        P_high_before5d[:3] = ci_high_before5d[:3]
+        # P_after5d[:3] = transition_probs_after5[:3]
+    elif comorbidity <= 3:
+        P_low_before5d[:3] = ci_low_before5d[3:6]
+        P_high_before5d[:3] = ci_high_before5d[3:6]
+    else:
+        P_low_before5d[:3] = ci_low_before5d[6:]
+        P_high_before5d[:3] = ci_high_before5d[6:]
+    
+    random_P_before5d = np.random.uniform(P_low_before5d, P_high_before5d)
+    return random_P_before5d
+
+
+def count_death_rate(P_ij, initial_state, duration):
+    values = np.array(initial_state, dtype=float)
+    for _ in range(duration):
+        values = np.dot(P_ij.T, values)
+    return values[4]
+
+
+def bootstrap(comorbidity, initial_state, duration, n_samples=1000, alpha=0.9):
+    samples = []
+    samples_res = []
+    for _ in range(n_samples):
+        samples.append(generate_trans_probs(comorbidity))
+        samples_res.append(count_death_rate(samples[-1], initial_state, duration))
+    samples_res = np.array(samples_res)
+    low_res, high_res = np.quantile(samples_res, [(1 - alpha) / 2, (1 + alpha) / 2])
+    low_sample_idx = np.argmin(np.abs(samples_res - low_res))
+    high_sample_idx = np.argmin(np.abs(samples_res - high_res))
+    print(low_res, samples_res[low_sample_idx])
+    print(high_res, samples_res[high_sample_idx])
+    return samples[low_sample_idx], samples[high_sample_idx]
